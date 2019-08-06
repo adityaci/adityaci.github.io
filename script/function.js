2d8f7b81e635d92aa25543ebb29d4dea84797d09
Aditya C.I <aditya.chandra.isdhianto@gmail.com>
2019-08-06 18:19:47 +0700

    upload lagi

---
+++ script/function.js
@@ -0,0 +1,35 @@
+function display( param ){
+	document.getElementById("content_container").classList.toggle("fade");
+	if( param == "about" )
+		document.getElementById("about").style.display = "block";
+	else if( param == "skill" )
+		document.getElementById("skill").style.display = "block";
+	else if( param == "portfolio" )
+		document.getElementById("portfolio").style.display = "block";
+	else
+		document.getElementById("contact").style.display = "block";
+}
+
+function closeAllContent(){
+	const all = document.getElementById("content_container").querySelectorAll(".content");
+	for( var i = 0; i < all.length; i++ )
+		all[i].style.display = "none";
+}
+
+function backToHome(){
+	closeAllContent();
+	document.getElementById("content_container").classList.toggle("fade");
+}
+
+function changeDialogue( param ){
+	var dialogue = "";
+	if( param == "about" )
+		dialogue = "Wanna know about me ? click it!";
+	else if( param == "skill" )
+		dialogue = "And.. that's my skills info, click it!";
+	else if( param == "portfolio" )
+		dialogue = "That's for my portfolio, click it!";
+	else
+		dialogue = "Contact me, hope we can have fun!";
+	document.getElementById("dialogue").innerHTML = dialogue;
+}
